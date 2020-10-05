d11997a236fdda3c930b6fcbfafcee0228fa0f8a
dmw1998 <dmw1998@users.noreply.github.com>
2020-10-05 16:47:04 +0800

    Update Ex0909.f90

--- CH15/补充材料/Ex0909.f90
+++ CH15/补充材料/Ex0909.f90
@@ -1,12 +1,12 @@
-program ex0909
+program Ex0909
   implicit none
   integer, parameter :: inputfileid = 10, outputfileid = 11
   integer, parameter :: maxbuffer = 200
   character(len=80)  :: inputfile, outputfile
-  character(len=maxbuffer) buffer
-  integer count
-  integer error
-  logical alive
+  character(len=maxbuffer) :: buffer
+  integer :: count
+  integer :: error
+  logical :: alive
   
   write(*,*) "Input Filename:"
   read(*,"(A80)") inputfile
@@ -21,15 +21,16 @@ program ex0909
   
   open(unit=inputfileid, file=inputfile, status="old")
   open(unit=outputfileid, file=outputfile, status="replace")
+
   count = 1
   do while(.true.)
     read(inputfileid,"(A200)",iostat=error) buffer
-	if ( error/=0 ) exit 
+	  if ( error/=0 ) exit 
     write(outputfileid, "(I3,'.',A)") count,trim(buffer)
-	count=count+1 
+	  count=count+1 
   end do
+
   close(inputfileid)
   close(outputfileid)
 
-  stop
-end program
+end program Ex0909
