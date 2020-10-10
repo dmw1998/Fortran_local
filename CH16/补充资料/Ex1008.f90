6244a77bf22fc9556c47109bbb046555b2aa6035
dmw1998 <dmw1998@users.noreply.github.com>
2020-10-10 18:58:50 +0800

    Create Ex1008.f90

---
+++ CH16/补充资料/Ex1008.f90
@@ -0,0 +1,35 @@
+module func
+
+contains 
+
+    function getmin(p)
+        implicit none
+        
+        integer, pointer :: p(:)
+        integer, pointer :: getmin
+        integer :: i, s, min
+        
+        s = size(p,1) 
+        min = 2**30       ! Set the minimum as a large number
+        do i=1,s
+            if (min > p(i)) then
+                min=p(i)
+                getmin=>p(i)
+            end if
+        end do
+    end function
+
+end module func
+
+program Ex1008
+use func
+implicit none
+
+integer, target  :: a(8)=(/ 10, 15, 8, 25, 9, 20, 17, 19 /)
+integer, pointer :: p(:)
+
+p => a(1:8:2)
+! p(1)=>a(1), p(2)=>a(3), p(3)=>a(5), p(4)=>a(7)
+write(*,*) getmin(p)
+
+end program Ex1008
